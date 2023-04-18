ee7c54823ad82eb261933c76b79e66dc420c0f1d
Amirul <naimteehee@gmail.com>
2023-04-18 23:28:38 +0800

    Install inertiajs including require svelte package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
-import './bootstrap';
+import { createInertiaApp } from '@inertiajs/svelte'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+createInertiaApp({
+    resolve: name => {
+        // return resolvePageComponent(`./Pages/${name}.svelte`, import.meta.glob(`./Pages/**/*.svelte`))
+        const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
+        return pages[`./Pages/${name}.svelte`]
+    },
+    setup({ el, App, props }) {
+        new App({ target: el, props })
+    },
+})
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
+import dotenv from 'dotenv'
+
+dotenv.config() // load env vars from .env
+
+const host = process.env.VITE_APP_URL;
+const port = process.env.VITE_PORT;
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
+        svelte({
+            preprocess: sveltePreprocess()
+        })
     ],
+    server: { 
+        host, 
+        hmr: { host }, 
+        port: port, 
+    }
 });
