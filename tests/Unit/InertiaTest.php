328a543c3ad8a2bd23d517d676b946cdb1d200e1
Amirul <naimteehee@gmail.com>
2023-04-19 17:43:48 +0800

    Update InertiaTest

--- tests/Unit/InertiaTest.php
+++ tests/Unit/InertiaTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Enums\ClaimStatusEnum;
+use App\Models\Claim;
 use Inertia\Testing\AssertableInertia as Assert;
 
 test('Base page return correct Svelte file', function () {
@@ -9,6 +11,12 @@ test('Base page return correct Svelte file', function () {
 test('Employee Dashboard page return correct Svelte file', function () {
     $this->get('/dashboard/employee')->assertInertia(fn (Assert $page) => $page->component('Employee/Dashboard'));
     $this->get('/dashboard/employee/submit')->assertInertia(fn (Assert $page) => $page->component('Employee/Submit'));
+
+    $claim = Claim::factory()->create([
+        'status' => ClaimStatusEnum::DRAFT,
+        'submitted_at' => null,
+    ]);
+    $this->get('/dashboard/employee/edit/'.$claim->id)->assertInertia(fn (Assert $page) => $page->component('Employee/Edit'));
 });
 
 test('Admin Dashboard page return correct Svelte file', function () {
