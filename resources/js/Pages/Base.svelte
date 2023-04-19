0cfa66eaf56a09b3ed42b78ad13d4f25550e9ad9
Amirul <naimteehee@gmail.com>
2023-04-19 21:05:22 +0800

    Fix base page styling

--- resources/js/Pages/Base.svelte
+++ resources/js/Pages/Base.svelte
@@ -5,7 +5,7 @@
 <div class="flex flex-row items-center justify-center py-20 px-20">
     <!-- centered content -->
   
-    <a href="/dashboard/employee" use:inertia class="text-white bg-blue-700 m-5 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-blue-500">
+    <a href="/dashboard/employee" use:inertia class="text-white bg-blue-700 m-5 block max-w-sm p-6 bg-blue border border-gray-200 rounded-lg shadow hover:bg-blue-500">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-white-900">Employee</h5>
         <p class="font-normal text-white-700">Here is dashboard for employee view. Employee are able to:</p>
         <ul class="list-disc ml-4 font-semibold">
