0cefb33dcc44b6cf730e4915b3d18276b12b83ad
Amirul <naimteehee@gmail.com>
2023-04-19 15:05:36 +0800

    Fix nagivation style

--- resources/js/Pages/Layouts/Navigations/Employee.svelte
+++ resources/js/Pages/Layouts/Navigations/Employee.svelte
@@ -6,4 +6,4 @@
 
 <a href="/dashboard/employee" use:inertia class="{current_navigation == 'employee_dashboard' ? 'bg-blue-900' : 'text-blue-300 hover:bg-blue-700'} text-white rounded-md px-3 py-2 {is_mobile ? 'block text-base' : 'text-sm'} font-medium">Dashboard</a>
                             
-<a href="/dashboard/employee/submit" use:inertia class="{current_navigation == 'employee_claim' ? 'bg-blue-900' : 'text-blue-300 hover:bg-blue-700'} hover:text-white rounded-md px-3 py-2 {is_mobile ? 'block text-base' : 'text-sm'} font-medium">Submit claim</a>
+<a href="/dashboard/employee/submit" use:inertia class="{current_navigation == 'employee_claim' ? 'bg-blue-900' : 'text-blue-300 hover:bg-blue-700'} text-white rounded-md px-3 py-2 {is_mobile ? 'block text-base' : 'text-sm'} font-medium">Submit claim</a>
