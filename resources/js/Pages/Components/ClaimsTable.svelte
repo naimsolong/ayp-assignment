a66e83dd63c92f98ae69afa57cd58cfc62d4b384
Amirul <naimteehee@gmail.com>
2023-04-19 15:34:20 +0800

    Fix button styling

--- resources/js/Pages/Components/ClaimsTable.svelte
+++ resources/js/Pages/Components/ClaimsTable.svelte
@@ -51,9 +51,9 @@
                 </td>
                 <td class="px-6 py-4 text-right">
                     {#if row.status == 'D' && row.submitted_at == null}
-                    <a href="/" class="font-medium text-blue-600 hover:underline">Edit</a>
+                    <a href="/" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">Edit</a>
                     {:else}
-                    <span class="font-medium text-grey-600">Edit</span>
+                    <span class="text-white bg-gray-400 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center" disabled>Edit</span>
                     {/if}
                 </td>
             </tr>
